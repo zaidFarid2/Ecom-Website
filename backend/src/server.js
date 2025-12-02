4483bf0ffd1a78e08dfc4ad6e7a34c7a2cb61a27
Muhammad Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2025-12-03 02:00:53 +0500

    Project Setup

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app  = express()
+
+app.get("/api/health",(req,res)=>{
+    res.status(200).json({message:"Success"})
+})
+
+app.listen( 3000 , ()=>console.log("app is run"))   
